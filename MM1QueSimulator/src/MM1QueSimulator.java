707356c0bff1779a0ca872560886549751330b6d
Nkosikhona Dlamini <ndlamini3@csir.co.za>
2017-04-13 14:36:37 +0200

    updating comments

--- MM1QueSimulator/src/MM1QueSimulator.java
+++ MM1QueSimulator/src/MM1QueSimulator.java
@@ -5,19 +5,21 @@ import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Random;
 import cern.jet.random.Poisson;
-import cern.jet.random.engine.*;
+import cern.jet.random.engine.RandomEngine;
+import cern.jet.random.engine.RandomSeedGenerator;
+import cern.jet.random.engine.DRand;
+
+
 
 public class MM1QueSimulator {
 
-	//private static Random random ;
-	//private static long seed; 
+	 
 		final static String FILENAME ="/home/hltuser/assigmmentFile.txt" ;
 		static Poisson poisson ;
 		static Poisson poisson2;
 		static RandomEngine engine;
-		static double serviceRate; // link/packet size
+		static double serviceRate; // this is link/packet size
 		static double arrivalRate; // percentage of service rate packets
 		static double averageInterArrivalRate; // 1/arrivalRate 
 		static double linkCapacity; // given link capacity
@@ -38,15 +40,34 @@ public class MM1QueSimulator {
 		  
 		  
 	 }
+	 /*
+	  * Computes service rate , as link capacity divided by package size average
+	  * @param lc , link capacity ,
+	  * @param packAv average packet size 
+	  * 
+	  *  returns service rate
+	  */
+	 
 	 public double calculateServiceRate(double lc,double packAv)
 	 {
 		 return (lc/packAv);
 	 }
+	 /*
+	  * Computes arrival work load based on percentage of service Rate  100% arrival = service rate , 10 % arrival = 10% of serviceRate , as link capacity divided by package size average
+	  * @param servRate , service rate ,
+	  * @param percentage percentage 10.0,  20.0...90. 
+	  *
+	  *  returns arrival Rate or workload
+	  */
 	 public double calculateArrivalRate(double servRate, double percentage)
 	 {
 		 return (percentage/100)*servRate;
 	 }
 	 
+	 /*Computes arrival Average 1/arrival Rate 
+	  * @param arrivalRate  ,arrvial rate . 
+	  * return arrival average in micro-seconds that is why its is multiplied by 1000000
+	  */
 	 public double calculateArrivalRateAverage(double arrivalRate)
 	 {
 		 return (1.0/arrivalRate)*1000000; 
@@ -67,6 +88,12 @@ public class MM1QueSimulator {
 		 
 	 }
 	
+	/*
+	 * PLEASE IGNORE THIS METHOD FOR NOW
+	 * MENANT FOR THE LAST PART OF THE PROJECT , NOT YET TESTED .
+	 * 
+	 */
+
 	public void readFromFile(String filename)
 	{
 		int totalPackets = 0;
@@ -155,6 +182,10 @@ public class MM1QueSimulator {
 		
 	}
 
+	/*Writes generated piosson distributed values to a file.
+	 * Not yet tested please ignore. For now output is printed out on consol 
+	 * 
+	 */
 	public static void writePoissonToFile(double packetLoad,double meanInterArrival, int meanPacketSize)
 	{
 		String content="";
@@ -178,14 +209,15 @@ public class MM1QueSimulator {
 	}
 	public static void main(String[] args)
 	{
-		
-		/* 10 % run 100 times, calculate queing delay lambda 
-		 * 20 % run 100 times
-		 * 30% 40% 50% 60% 70% 80% 90%
-		 * In each calculate the average arrival rate based on average service rate . use 1000 bits as mean for average packet size. 
-		 */
+		//Initialize  MM1QueSimulator object using default constructor
 		
 		MM1QueSimulator myQueSimulator = new MM1QueSimulator();
+		
+		/* 10 % run 100 times, 
+		 * 20 % run 100 times....90% ect 
+		 * In each of the 100 runs create a new random engine , with a new seed. this can be optimized
+		 * 
+		 */
 		for(double i = 10.0; i<=90.0;i+=10.0)
 		{
 			arrivalRate =myQueSimulator.calculateArrivalRate(serviceRate, i);
